de3a979f7b677dda81108335aa79caf50bbcb255
jackjavi <jackmtembete@gmail.com>
2022-08-16 12:26:51 +0300

    refactored code

---
+++ models/Task.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  pass: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
