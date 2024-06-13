2c4f6ea5cb1eda13f7098a26c82be36dab073d23
jackjavi <jackmtembete@gmail.com>
2024-06-13 11:29:25 +0300

    timestamps

--- models/Instagram.js
+++ models/Instagram.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const igSchema = new mongoose.Schema({
-  user: {
-    type: String,
-    trim: true,
-    required: true,
+const igSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    pass: {
+      type: String,
+      required: true,
+    },
   },
-  pass: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Instagram", igSchema);
--- models/Twitter.js
+++ models/Twitter.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const ttSchema = new mongoose.Schema({
-  user: {
-    type: String,
-    trim: true,
-    required: true,
+const ttSchema = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    pass: {
+      type: String,
+      required: true,
+    },
   },
-  pass: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Twitter", ttSchema);
